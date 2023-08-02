e16182e18da544d99c55bda38a5530f9be424610
Александр Филиппов <126808349+Axel1F@users.noreply.github.com>
2023-08-02 23:36:15 +0300

    Update dbo.vw_SKUPrice.sql

--- VIEWs/dbo.vw_SKUPrice.sql
+++ VIEWs/dbo.vw_SKUPrice.sql
@@ -1,12 +1,4 @@
---DROP VIEW SKUPrice;
-
---Use TestDb
---Go
-
 CREATE VIEW SKUPrice AS 
 SELECT *, [Price]=([dbo].[GetSKUPrice]([ID identity]))
 FROM dbo.SKU
 
-
---Создать представление (на выходе: файл в репозитории dbo.vw_SKUPriceв ветке VIEWs)
---4.1 Возвращает все атрибуты продуктов из таблицы dbo.SKU и расчетный атрибут со стоимостью одного продукта (используя функцию dbo.udf_GetSKUPrice)
