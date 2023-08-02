be49a10a02715d3731bb7b1c7c22deaefabb7a94
Александр Филиппов <126808349+Axel1F@users.noreply.github.com>
2023-08-02 23:34:57 +0300

    Update dbo.usp_MakeFamilyPurchase.sql

--- Procedures/dbo.usp_MakeFamilyPurchase.sql
+++ Procedures/dbo.usp_MakeFamilyPurchase.sql
@@ -1,5 +1,3 @@
---USE [TestDb]
---GO
 
 CREATE PROCEDURE MakeFamilyPurchase
 (
@@ -17,12 +15,6 @@ SET [BudgetValue] = ((SELECT BudgetValue FROM dbo.Family WHERE [SurName] = @Fami
 Where [SurName]= @FamilySurName;
 END
 
-Else print ('ошибка, такой семьи нет')
+Else print ('РѕС€РёР±РєР°, С‚Р°РєРѕР№ СЃРµРјСЊРё РЅРµС‚')
 END
 
-
---Создать процедуру (на выходе: файл в репозитории dbo.usp_MakeFamilyPurchase в ветке Procedures
---5.1 Входной параметр (@FamilySurName varchar(255)) одно из значений атрибута SurName таблицы dbo.Family
---5.2 Процедура при вызове обновляет данные в таблицы dbo.Family в поле BudgetValue по логике
---   5.2.1 dbo.Family.BudgetValue - sum(dbo.Basket.Value), где dbo.Basket.Value покупки для переданной в процедуру семьи
---   5.2.2 При передаче несуществующего dbo.Family.SurName пользователю выдается ошибка, что такой семьи нет
