7527225103752225c85d669fc4ab89e31e68b762
Александр Филиппов <126808349+Axel1F@users.noreply.github.com>
2023-08-02 23:34:39 +0300

    Update dbo.udf_GetSKUPrice.sql

--- Functions/dbo.udf_GetSKUPrice.sql
+++ Functions/dbo.udf_GetSKUPrice.sql
@@ -1,6 +1,3 @@
---Use TestDb
---Go
-
 CREATE FUNCTION GetSKUPrice (@ID_SKU INT)
     RETURNS DECIMAL(18, 2)
     BEGIN
@@ -11,9 +8,3 @@ CREATE FUNCTION GetSKUPrice (@ID_SKU INT)
         RETURN @addPrice
     END;
 
-
---Создать функцию (на выходе: файл в репозитории dbo.udf_GetSKUPrice.sql в ветке Functions)
---3.1 Входной параметр @ID_SKU
---3.2 Рассчитывает стоимость передаваемого продукта из таблицы dbo.Basket по формуле
---   3.1.1 сумма Value по переданному SKU / сумма Quantity по переданному SKU
---3.3 На выходе значение типа decimal(18, 2)
