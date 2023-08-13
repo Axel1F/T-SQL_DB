8bb30a74af24777479467b85f8ea0c6e8e5a40a9
Александр Филиппов <126808349+Axel1F@users.noreply.github.com>
2023-08-13 17:54:56 +0300

    Update dbo.TR_Basket_insert_update.sql

--- Triggers/dbo.TR_Basket_insert_update.sql
+++ Triggers/dbo.TR_Basket_insert_update.sql
@@ -9,23 +9,23 @@ ON dbo.Basket
 AFTER INSERT
 AS
 BEGIN
-UPDATE dbo.Basket
-SET [DiscountValue]=Value * 5/100
-WHERE [ID identity] IN (SELECT [ID identity] 
-                        FROM inserted 
-						WHERE [ID_SKU] IN (SELECT [ID_SKU] 
-						                   FROM inserted 
-										   GROUP BY [ID_SKU] 
-										   HAVING COUNT([ID_SKU])>1) 
-			            GROUP BY [ID identity])
-UPDATE dbo.Basket
-SET [DiscountValue]=0
-WHERE [ID identity] IN (SELECT [ID identity] 
-                        FROM inserted 
-						WHERE [ID_SKU] IN (SELECT [ID_SKU] 
-						                   FROM inserted 
-										   GROUP BY [ID_SKU] 
-										   HAVING COUNT([ID_SKU])=1) 
-						GROUP BY [ID identity])
+     UPDATE dbo.Basket
+     SET [DiscountValue]=Value * 5/100
+     WHERE [ID identity] IN (SELECT [ID identity] 
+                             FROM inserted 
+			     WHERE [ID_SKU] IN (SELECT [ID_SKU] 
+					        FROM inserted 
+					        GROUP BY [ID_SKU] 
+					        HAVING COUNT([ID_SKU])>1) 
+		             GROUP BY [ID identity])
+     UPDATE dbo.Basket
+     SET [DiscountValue]=0
+     WHERE [ID identity] IN (SELECT [ID identity] 
+                             FROM inserted 
+			     WHERE [ID_SKU] IN (SELECT [ID_SKU] 
+						FROM inserted 
+						GROUP BY [ID_SKU] 
+						HAVING COUNT([ID_SKU])=1) 
+			     GROUP BY [ID identity])
 END
 
