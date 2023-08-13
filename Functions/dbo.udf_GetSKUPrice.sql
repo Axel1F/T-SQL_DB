6fbed43e00c1e6dc6bfdca03a259dd889758a4b3
Александр Филиппов <126808349+Axel1F@users.noreply.github.com>
2023-08-13 17:55:42 +0300

    Update dbo.udf_GetSKUPrice.sql

--- Functions/dbo.udf_GetSKUPrice.sql
+++ Functions/dbo.udf_GetSKUPrice.sql
@@ -6,8 +6,8 @@ CREATE FUNCTION GetSKUPrice (@ID_SKU INT)
     BEGIN
         DECLARE @addPrice DECIMAL(18, 2)
         SELECT @addPrice = [Value]/ [Quantity]
-		FROM dbo.Basket 
-		where ID_SKU = @ID_SKU
+	FROM dbo.Basket 
+	where ID_SKU = @ID_SKU
         RETURN @addPrice
     END;
 
