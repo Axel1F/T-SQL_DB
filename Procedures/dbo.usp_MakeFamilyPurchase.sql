a834bfe5732a4ea88b17d45e1e6f3fb205536ade
Александр Филиппов <126808349+Axel1F@users.noreply.github.com>
2023-08-13 17:52:08 +0300

    Update dbo.usp_MakeFamilyPurchase.sql

--- Procedures/dbo.usp_MakeFamilyPurchase.sql
+++ Procedures/dbo.usp_MakeFamilyPurchase.sql
@@ -9,17 +9,17 @@ AS
 BEGIN
 IF @FamilySurName IN (SELECT SurName FROM dbo.Family)
      BEGIN
-	      DECLARE @SumValue INT
-		  SELECT @SumValue = sum([Value]) 
+	  DECLARE @SumValue INT
+	  SELECT @SumValue = sum([Value]) 
                              from dbo.Basket
-							 where dbo.Basket.ID_Family = (SELECT [ID identity] 
-				                                           FROM dbo.Family
-														   WHERE [SurName] = @FamilySurName)
-          UPDATE dbo.Family
-		  SET [BudgetValue] = ((SELECT BudgetValue 
+			     where dbo.Basket.ID_Family = (SELECT [ID identity] 
+				                           FROM dbo.Family
+							   WHERE [SurName] = @FamilySurName)
+           UPDATE dbo.Family
+	   SET [BudgetValue] = ((SELECT BudgetValue 
                                 FROM dbo.Family
-								WHERE [SurName] = @FamilySurName) - @SumValue)
-	      Where [SurName]= @FamilySurName;
+				WHERE [SurName] = @FamilySurName) - @SumValue)
+	    Where [SurName]= @FamilySurName;
 	 END
 
 Else print ('ошибка, такой семьи нет')
